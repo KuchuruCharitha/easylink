cab1867e186b5557683c41219edaac48d4c03723
jayasimhareddy27 <cherryyashu369@gmail.com>
2022-12-12 01:24:03 +0530

    commit18

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
 import express  from "express";
 import cors from 'cors';
-
 import authRoutes from './routes/auth.js';
-
 import bodyParser from "body-parser";
-
 import dotenv from 'dotenv';
-
 import twilio from 'twilio';
-
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,15 +14,17 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const twilioClient=new twilio(accountSid, authToken);
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-app.use(express.static('public'))
 
 app.post('/', (req, res) => {
     const { message, user: sender, type, members } = req.body;
