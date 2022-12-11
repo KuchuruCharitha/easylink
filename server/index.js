3dafd0429cc723a0610dcfa61f29ffd7bcd56d6c
jayasimhareddy27 <cherryyashu369@gmail.com>
2022-12-12 00:33:57 +0530

    commit11

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import twilio from 'twilio';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 dotenv.config();
 
@@ -20,7 +21,6 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const twilioClient=new twilio(accountSid, authToken);
-app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
